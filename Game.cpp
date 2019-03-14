b626e5f6809117a23419e94a97be5fcf367c712d
Pierre Gillet <pierre.gillet@linuxw.info>
2019-03-14 18:52:00 +0000

    Removed useless print

--- Game.cpp
+++ Game.cpp
@@ -91,7 +91,6 @@ void Game::initializeGlew() {
         std::cout << "glewInit failed, aborting." << std::endl;
         exit(1);
     }
-    std::cout << glGetString(GL_VERSION) << std::endl;
 }
 
 void Game::setupRenderingContext() {
